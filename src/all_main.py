52e107b2e02789244a4c7bb911268e7e229b0463
AbnerTeng <abnerteng16@gmail.com>
2024-12-18 15:09:36 +0800

    dev: construct non-rolling strategy

--- src/all_main.py
+++ src/all_main.py
@@ -9,8 +9,8 @@ from typing import Union
 from argparse import ArgumentParser, Namespace
 import numpy as np
 
-from .strategy import Strategy, RunRollingStrategy
-from .utils.general_utils import load_config, generate_random_param, get_class
+from .strategy import Strategy, RunStrategy
+from .utils.general_utils import load_config, get_class
 from .utils.data_utils import transfer_colnames
 from .get_data import DataAPI
 from .base.base_indicator import GlobalDataManager
@@ -40,7 +40,6 @@ if __name__ == "__main__":
     data = fetcher.fetch(api_map[args.data_source])
     df = transfer_colnames(data)
     GlobalDataManager.set_data(df)
-    i, val_year, last_year = 1, 0, df.index[-1].year
     full_log, full_trajectory, full_return_log, full_date, full_param, full_transac_cumret = (
         [],
         [],
@@ -51,63 +50,25 @@ if __name__ == "__main__":
     )
     ModelClass = get_class(cfg.Class.strat)
     ind = ModelClass(*cfg.Class.params)
-    last_idx, half_ids, BEST_CLASS = None, [], ind
+    last_idx, half_ids = None, []
     ind: Union[BB, EMA, RSI, SMA]
 
-    while val_year < last_year:
-        best_ret = -np.inf
+    if not RANDOM_PARAM:
+        cfg.trials = 1
 
-        if not RANDOM_PARAM:
-            cfg.trials = 1
+    val_df = ind.build()
+    strat_val = Strategy(val_df, ind)
+    valid_runner = RunStrategy(val_df, strat_val, **cfg.Settings)
+    ret = valid_runner.run(0, val_df.shape[0] - 1)
+    period_param = {ind.tag: ind.get_init_args()}
+    full_log.append(valid_runner.trade_log)
+    full_trajectory.extend(valid_runner.trajectory)
+    full_return_log.extend(valid_runner.return_log["return"])
+    full_date.extend(valid_runner.return_log["date"])
+    full_param.extend(period_param)
+    full_transac_cumret.extend(valid_runner.return_log["transac_cumret"])
 
-        for _ in range(cfg.trials):
-            num_params = ind.num_args()
-            bound_args = ind.get_init_args()
-
-            if RANDOM_PARAM:
-                ind = generate_random_param(ind, bound_args)
-
-            train_df = ind.build()
-            spec_strategy = Strategy(train_df, ind)
-            train_runner = RunRollingStrategy(train_df, spec_strategy, **cfg.Settings)
-            last_ids, half_ids = train_runner.day_of_year_idx()
-
-            if i == 1:
-                last_idx = last_ids[0]
-
-            ret, val_year_stop = train_runner.run(0, last_idx)
-
-            if ret > best_ret:
-                best_ret = ret
-                BEST_CLASS = spec_strategy.indicator
-
-            GlobalDataManager.reset()
-
-        print(f"training period {i - 1} | Cum Ret: {best_ret}")
-        val_start_idx = last_idx
-        i += 1
-
-        BEST_CLASS: Union[BB, EMA, RSI, SMA]
-        val_df = BEST_CLASS.build()
-        combine_strat_val = Strategy(val_df, BEST_CLASS)
-        valid_runner = RunRollingStrategy(val_df, combine_strat_val, **cfg.Settings)
-
-        if i == len(half_ids) - 2:
-            ret, val_year = valid_runner.run(val_start_idx, val_df.shape[0] - 1)
-        else:
-            ret, val_year = valid_runner.run(val_start_idx, half_ids[i])
-
-        last_idx = half_ids[i]
-        print(f"Validation period {i - 1} | Cum Ret: {ret}")
-        period_param = {ind.tag: ind.get_init_args()}
-        full_log.append(valid_runner.trade_log)
-        full_trajectory.extend(valid_runner.trajectory)
-        full_return_log.extend(valid_runner.return_log["return"])
-        full_date.extend(valid_runner.return_log["date"])
-        full_param.extend(period_param)
-        full_transac_cumret.extend(valid_runner.return_log["transac_cumret"])
-
-        GlobalDataManager.reset()
+    GlobalDataManager.reset()
 
     dic = {
         "date": full_date,
@@ -117,7 +78,7 @@ if __name__ == "__main__":
         "trajectory": full_trajectory,
         "param": full_param,
     }
-    print(np.cumsum(dic["return"])[-1])
+    print(f"Cum ret: {np.cumsum(dic['return'])[-1]}")
 
     if cfg.task == "syn_spsl":
         pkl_filename = f"{str(ind)}_{cfg.Settings.sl_thres}_{cfg.Settings.sp_thres}.pkl"
